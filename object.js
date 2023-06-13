45ef45d11a0cbb33fd3b1df2cb717cb53ceee7ef
Palanivel22 <136297406+Palanivel22@users.noreply.github.com>
2023-06-13 14:55:09 +0530

    add file

---
+++ object.js
@@ -0,0 +1,11 @@
+var a=[{name:"kavi",age:18},
+{boy:"kavi",rollno:39},
+{name:"kavi",village:"karayapalayam"},
+{name:"kavi",group:"o+"},
+{bike:"R15",price:200000},
+{tree:"mangotree",fruit:"mango"},
+{fruit:'apple',rate:150},
+{name:"onepiece",episode:1065},
+{name:"onepiece",episode:1085},
+{character:"goku",strenght:"mui"}]
+console.log(a)
---
+++ reduse.js
@@ -0,0 +1,6 @@
+function sum(...args){
+return args.reduse(total.args) =+ total+arg,0;
+}
+console.log(sum(1,4));
+console.log(sum(2));
+console.log(sum());
